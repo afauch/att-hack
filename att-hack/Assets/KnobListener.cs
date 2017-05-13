b86901697237ccb737ea3b91719ca48b0ab43baa
Aaron Faucher <aaron.faucher@gmail.com>
2017-05-13 13:03:28 -0700

    KnobListener on Update added

---
+++ att-hack/Assets/KnobListener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class KnobListener : MonoBehaviour {
+
+	public int _knobNumber;
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if(MidiMaster.GetKnob(MidiChannel.Ch1, _knobNumber, 0.0f) > 0.0f) {
+			float knobValue = MidiMaster.GetKnob (MidiChannel.Ch1, _knobNumber, 0.0f);
+			Debug.Log (knobValue);
+		}
+		
+	}
+}
